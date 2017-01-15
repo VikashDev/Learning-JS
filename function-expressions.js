1cd1bee507dda1ea8d76d9469d646e7239fc736f
Ashok Dey <ashokdey100@gmail.com>
2017-01-15 15:18:21 +0530

    added examples

--- function-expressions.js
+++ function-expressions.js
@@ -10,5 +10,15 @@ console.log(sum(10, 20));
  *  */
 
 let square = (a) => a * a;  // ES6 arrow function
+let double = (a) => a/2;  // ES6 arrow function
 
 console.log(square(10));
+
+// make a function that takes 3 parameters (2 numbers, one operation) 
+// the operatoion cam be anything. 
+// inside the function, perform the operation on the  2 numbers and return their sum
+
+let result = (a, b, op) =>  op(a) + op(b);
+
+console.log(result(2, 3, square));
+console.log(result(2, 3, double));
