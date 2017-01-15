5bf6df12c6a447a5239ce422ff2fcb85671d6516
Vikash Kumar <vikash087@gmail.com>
2017-01-16 00:11:44 +0530

    Function example..

---
+++ practice/function.js
@@ -0,0 +1,12 @@
+// creating basic calculater by using funtions
+
+'use strict';
+
+
+
+let result = (a, b , op) => {
+    return result = ((op == 1) ? (a + b) : (a * b));
+}
+
+// console.log(result(2, 4, 1));
+console.log(result(2, 4, 2));
