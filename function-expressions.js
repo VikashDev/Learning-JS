a38137ace681a1534e71dcde34cea7fb817a6516
Vikash Kumar <VikashDev@users.noreply.github.com>
2017-01-17 15:18:54 +0530

    Update function-expressions.js

--- function-expressions.js
+++ function-expressions.js
@@ -1,5 +1,22 @@
 // in js, functions are first class objects which means we can assign a function to a variable
 
+//anonymous function expression
+var a = function() {
+    return 3;
+}
+ 
+//named function expression
+var a = function bar() {
+    return 3;
+}
+ 
+//self invoking function expression
+(function sayHello() {
+    alert("hello!");
+})();
+
+//Examples
+
 let sum = function (a,b) {return a+b}
 console.log(sum(10, 20));
 //console.log(add(10, 20)); there is no need of giving a name to a function when you are assigning it to a variiable
